5708e02e3a9b77aaa48353287967d09ccbf87915
Pedro "Spark" Fernandes <42358861+PedroFernandesSpark@users.noreply.github.com>
2021-08-26 12:06:18 -0300

    Melhoria das funçoes com mascaras

--- main.c
+++ main.c
@@ -22,12 +22,16 @@ int32_t		imm12_i, imm12_s, imm13, imm20_u, imm21;
 
 // Memory access func
 int8_t end = 0;
-int32_t lw(uint32_t address, int32_t kte);
+uint32_t lw(uint32_t address, int32_t kte);
 int32_t lb(uint32_t address, int32_t kte);
 int32_t lb(uint32_t address, int32_t kte);
 int32_t lbu(uint32_t address, int32_t kte);
 void sw(uint32_t address, int32_t kte, int32_t dado);
 void sb(uint32_t address, int32_t kte, int8_t dado);
+int32_t imm_format_i(int32_t imm);
+int32_t imm_format_s(int32_t imm_1,int32_t imm_2);
+int32_t imm_format_sb(int32_t imm_1,int32_t imm_2);
+int32_t imm_format_uj(int32_t imm);
 
 // Binary convert func
 int load_mem();
@@ -44,27 +48,28 @@ void execute();
 void run();
 
 int main() {
-    run();
     int load_mem_status = load_mem();
-    printf("Load_mem returned status code: %i \n\n", load_mem_status);
+    run();
+    //printf("Load_mem returned status code: %i \n\n", load_mem_status);
 
-    printf("Registers list:\n");
-    dump_reg('h');
+    //printf("Registers list:\n");
+    //dump_reg('h');
 
-    printf("Memory dump:\n");
-    dump_mem(0, 4096, 'h');
+    //printf("Memory dump:\n");
+    //dump_mem(0, 4096, 'h');
 
     return 0;
 }
 
 void fetch(){
-    ri = lw(pc,0);
+    ri = lw(pc, 0);
     pc += 4;
 }
 
 void run(){
     while(!end && pc < 20000){
         step();
+
     }
 }
 
@@ -72,6 +77,7 @@ void step(){
     fetch();
     decode();
     execute();
+    return;
 }
 
 void decode(){
@@ -79,39 +85,107 @@ void decode(){
     opcode = buff&0b00000000000000000000000001111111;
     if(opcode < 0b0100011 && opcode > 0000010 || opcode == 1110011 || opcode == 1100111){ // I
         rd     = buff&0b00000000000000000000111110000000;
+        rd     = rd >> 7;
         funct3 = buff&0b00000000000000000111000000000000;
+        funct3 = funct3 >> 12;
         rs1    = buff&0b00000000000011111000000000000000;
+        rs1    = rs1 >> 15;
         imm12_i= buff&0b11111111111100000000000000000000;
+        imm12_i= imm_format_i(imm12_i);
     }
     if(opcode < 0111100 && opcode > 00110010 && opcode != 0110111){ // R
         rd     = buff&0b00000000000000000000111110000000;
+        rd     = rd >> 7;
         funct3 = buff&0b00000000000000000111000000000000;
+        funct3 = funct3 >> 12;
         rs1    = buff&0b00000000000011111000000000000000;
+        rs1    = rs1 >> 15;
         rs2    = buff&0b00000001111100000000000000000000;
+        rs2    = rs2 >> 20;
         funct7 = buff&0b11111110000000000000000000000000;
-    }
-    if(opcode == 1100011){ //SB
-        imm12_s    = buff&0b00000000000000000000111110000000;
-        funct3 = buff&0b00000000000000000111000000000000;
-        rs1    = buff&0b00000000000011111000000000000000;
-        rs2    = buff&0b00000001111100000000000000000000;
-        imm13 = buff&0b11111110000000000000000000000000;
     }
     if(opcode == 0100011){ //S
         imm12_s= buff&0b00000000000000000000111110000000;
         funct3 = buff&0b00000000000000000111000000000000;
+        funct3 = funct3 >> 12;
         rs1    = buff&0b00000000000011111000000000000000;
+        rs1    = rs1 >> 15;
         rs2    = buff&0b00000001111100000000000000000000;
+        rs2    = rs2 >> 20;
+        imm13  = buff&0b11111110000000000000000000000000;
+        imm13  = imm_format_s(imm12_s,imm13);
+    }
+    if(opcode == 1100011){ //SB
+        imm12_s= buff&0b00000000000000000000111110000000;
+        funct3 = buff&0b00000000000000000111000000000000;
+        funct3 = funct3 >> 12;
+        rs1    = buff&0b00000000000011111000000000000000;
+        rs1    = rs1 >> 15;
+        rs2    = buff&0b00000001111100000000000000000000;
+        rs2    = rs2 >> 20;
         imm13 = buff&0b11111110000000000000000000000000;
+        imm13 = imm_format_sb(imm12_s,imm13);
     }
     if(opcode == 1101111){ //UJ
         rd     = buff&0b00000000000000000000111110000000;
+        rd     = rd >> 7;
         imm21  = buff&0b11111111111111111111000000000000;
+        imm21  = imm_format_uj(imm21);
     }
     else{
         rd     = buff&0b00000000000000000000111110000000;
+        rd     = rd >> 7;
         imm20_u= buff&0b11111111111111111111000000000000;
     }
+}
+
+int32_t imm_format_i(int32_t imm){
+    imm = imm & 0xfff00000;
+    imm = imm >> 20;
+    return imm;
+}
+int32_t imm_format_uj(int32_t imm){
+    int32_t imm_1, imm_2, imm_3, imm_4, imm_full;
+    imm_full = 0b11111111111111111111111111111111;
+    imm_1 = imm & 0b00000001111000000000000000000000;
+    imm_1 = imm_1 >> 21;
+    imm_2 = imm & 0b01111110000000000000000000000000;
+    imm_2 = imm_2 >> 21;
+    imm_3 = imm & 0b00000000000000000001000000000000;
+    imm_3 = imm_3 >> 21;
+    imm_4 = imm & 0b10000000000000000000000000000000;
+    imm_4 = imm_4 >> 21;
+    imm_full = imm_full & imm_1;
+    imm_full = imm_full & imm_2;
+    imm_full = imm_full & imm_3;
+    imm_full = imm_full & imm_4;
+    return imm_full;
+}
+int32_t imm_format_s(int32_t imm_1,int32_t imm_2){
+    int32_t imm;
+    imm = 0b11111111111111111111111111111111;
+    imm_1 = imm_1 >> 7;
+    imm_2 = imm_2 >> 20;
+    imm = imm & imm_1;
+    imm = imm & imm_2;
+    return imm;
+}
+int32_t imm_format_sb(int32_t imm_1,int32_t imm_2){
+    int32_t imm;
+    int32_t imm_3,imm_4;
+    imm = 0b11111111111111111111111111111111;
+    imm_1 = imm_1 >> 7;
+    imm_3 = imm_1 & 0b00000000000000000000000000000001;
+    imm_1 = imm_1 & 0b00000000000000000011111111111110;
+    imm_2 = imm_2 >> 20;
+    imm_4 = imm_2 & 0b00000000000000000001000000000000;
+    imm_3 = imm_3 << 10;
+    imm_4 = imm_4 << 1;
+    imm = imm & imm_1;
+    imm = imm & imm_2;
+    imm = imm & imm_3;
+    imm = imm & imm_4;
+    return imm;
 }
 
 void execute(){
@@ -226,9 +300,21 @@ void execute(){
     if(opcode == 0b0110011 && funct3 == 0b100 && funct7 == 0b0000000){ // xor
         mem[rd] = mem[rs1] ^ mem[rs2];
     }
-    if(opcode == 0b1110011 && funct3 == 0b000 && funct7 == 0b0000000){ // xor
-        end = 1;
-        printf("Risc-V returned 0");
+    if(opcode == 0b1110011 && funct3 == 0b000 && funct7 == 0b0000000){ // Syscall
+        if(imm12_i == 1){
+            printf("%i", mem[0x10]);
+            printf("\n");
+        }
+        if(imm12_i == 8){
+            for (int i=0; i < mem[0x11]; i++){
+                printf("%c", mem[0x11+i]);
+            }
+            printf("\n");
+        }
+        if(imm12_i == 10){
+            end = 1;
+            printf("Risc-V returned 0");
+        }
     }
 }
 
@@ -249,6 +335,10 @@ void dump_reg(char format){
         printf("RI => %X \n", ri);
         printf("SP => %X \n", sp);
         printf("GP => %X \n", gp);
+        printf("OPCODE => %X \n", opcode);
+        printf("RD => %X \n", rd);
+        printf("RS1 => %X \n", rs1);
+        printf("RS2 => %X \n", rs2);
     }
     if(format == 'd'){
         printf("PC => %d \n", pc);
@@ -279,10 +369,11 @@ int load_mem(){
     return 0;
 }
 
-int32_t lw(uint32_t address, int32_t kte){
+uint32_t lw(uint32_t address, int32_t kte){
     uint32_t add = address+kte;
-    if(add % 4 == 0)
+    if(add % 4 == 0){
         return mem[add/4];
+    }
     else{
         printf("O valor de Address + Kte deve ser um multiplo de 4\n");
         return 0;
